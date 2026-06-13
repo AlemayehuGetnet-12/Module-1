7d14f6e258e4c209abecb6a6601b526e8f1c1dbd
Alemayehu Getnet <alexgetnet34l@example.com>
2026-06-13 02:02:41 -0700

    Initial project use cs

---
+++ look.cs
@@ -0,0 +1,7 @@
+public class Student
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public decimal GPA { get; set; }
+}
